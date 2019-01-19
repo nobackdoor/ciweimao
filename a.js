2abb50782fc9edf8d7034bf44489cd349fba0b04
Venn <36472129+MuoDoo@users.noreply.github.com>
2019-01-19 21:55:20 +0800

    Update a.js

--- a.js
+++ a.js
@@ -1,4 +1,4 @@
-functin fuck() {
-document.getElementsByTagName(`body`)[0].appendChild(createElement(`iframe`)).src=`//baidu.com`;
-}
-fuck();
+function fuck() {
+			document.getElementsByTagName(`body`)[0].appendChild(document.createElement(`iframe`)).src = '//baidu.com';
+		}
+		fuck();
